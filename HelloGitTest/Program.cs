d37f14174576cede71b2f62d0b4e68e71048d5a3
Muhammad Zakria Shahid <muhammad.shahid@edu.tbz.ch>
2024-02-06 10:44:07 +0100

    Projektdateien hinzufügen.

---
+++ HelloGitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+        }
+    }
+}
